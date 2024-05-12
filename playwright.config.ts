e0196dda473e1fea2110d90fbcc94e0a059272bc
Sam James <samjojames@gmail.com>
2024-05-12 12:30:50 +0100

    setup before e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,4 +69,10 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  webServer: {
+    command: `npm run start`,
+    url: "http://localhost:3000",
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
